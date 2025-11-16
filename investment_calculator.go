c29b7834952a05dd3435a5a1ae4226ff1d48573a
Adam Lopez <adam.lopez@hotwiremail.com>
2025-11-15 19:10:38 -0500

    using fmt.Print to display a message to the user that they should enter a value. this performs calculations based on investment amounts, years, and expected return rate the user enters when running the go program.

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,14 +6,22 @@ import (
 )
 
 func main() {
-	const inflationRate = 2.5    // never change this value
-	var investmentAmount float64 // you can declare a varible with no value - go automatically allows (doesn't work with constants)
-	years := 10.0                // Go infers these are float64
+	const inflationRate = 2.5 // never change this value
+	var investmentAmount,
+		years float64 // you can declare a varible with no value - go automatically allows (doesn't work with constants)
 	expectedReturnRate := 5.5
 
+	// Show a user a message
+	fmt.Print("Investment Amount: ")
 	// Gets inputs from the terminal
 	// need to add a 'pointer' in front of the variable
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years of Investment: ")
+	fmt.Scan(&years)
 
 	futureValue := float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
